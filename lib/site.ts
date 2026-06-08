9fc1058453775969693f6bdfc441eda126e6b062
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-06-08 02:54:38 +0100

    Create site configuration for Banner Of Excellence Schools
    Added site configuration for Banner Of Excellence Schools.

---
+++ lib/site.ts
@@ -0,0 +1,30 @@
+export const siteConfig = {
+  name: "Banner Of Excellence Schools",
+
+  description:
+    "Banner Of Excellence Schools is a leading early childhood education institution in Akwa Ibom State, Nigeria, offering Crèche, Nursery, and Primary education focused on excellence and holistic development.",
+
+  url: "https://school-website-sandy-xi.vercel.app",
+
+  ogImage: "/og-image.png",
+
+  address:
+    "No.12 Udo Ebu Street/Inyang Effe Street, Ekit Itam II, Itu L.G.A, Akwa Ibom State, Nigeria",
+
+  phone: [
+    "08035732076",
+    "08029168211",
+  ],
+
+  email: "banner@excellenceschools.com",
+
+  keywords: [
+    "School in Akwa Ibom",
+    "Primary School in Itu",
+    "Nursery School in Akwa Ibom",
+    "Crèche in Itu",
+    "Banner Of Excellence Schools",
+    "Best School in Itu LGA",
+    "Early Childhood Education Nigeria",
+  ],
+};
