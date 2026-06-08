802309ef8d2017625c596cfc5237132c25d80321
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-06-08 03:11:30 +0100

    Add StructuredData component for JSON-LD schema

---
+++ app/StructuredData.tsx
@@ -0,0 +1,38 @@
+import { siteConfig } from "@/lib/site";
+
+export default function StructuredData() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "School",
+    name: siteConfig.name,
+    description: siteConfig.description,
+    url: siteConfig.url,
+
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: siteConfig.address,
+      addressCountry: "NG",
+    },
+
+    contactPoint: [
+      {
+        "@type": "ContactPoint",
+        telephone: siteConfig.phone,
+        contactType: "Admissions",
+        email: siteConfig.email,
+        areaServed: "NG",
+      },
+    ],
+
+    sameAs: [],
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(jsonLd),
+      }}
+    />
+  );
+}
